5afca078f531c849a8f57f4bd90524bf51eea8b2
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-03-08 12:52:30 -0600

    lenguage

--- application/language/english/blog_informativo_lang.php
+++ application/language/english/blog_informativo_lang.php
@@ -14,5 +14,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $lang['blog'] = "Blog";
 
+$lang['title_question_one'] = "Blog";
+$lang['answer_question_one'] = "Blog";
+
+$lang['title_question_two'] = "Blog";
+$lang['answer_question_two'] = "Blog";
+
+$lang['title_question_three'] = "Blog";
+$lang['answer_question_three'] = "Blog";
+
+$lang['title_question_four'] = "Blog";
+$lang['answer_question_four'] = "Blog";
+
+$lang['title_question_five'] = "Blog";
+$lang['answer_question_five'] = "Blog";
+
+$lang['title_question_six'] = "Blog";
+$lang['answer_question_six'] = "Blog";
+
+$lang['title_question_seven'] = "Blog";
+$lang['answer_question_seven'] = "Blog";
+
+$lang['title_question_eight'] = "Blog";
+$lang['answer_question_eight'] = "Blog";
+
+$lang['title_question_nine'] = "Blog";
+$lang['answer_question_nine'] = "Blog";
+
+$lang['title_question_ten'] = "Blog";
+$lang['answer_question_ten'] = "Blog";
+
+$lang['title_question_one'] = "Blog";
+$lang['answer_question_one'] = "Blog";
+
+$lang['title_question_one'] = "Blog";
+$lang['answer_question_one'] = "Blog";
+
+
+
 $lang['english']="English";
 $lang['spanish']="Spanish";
