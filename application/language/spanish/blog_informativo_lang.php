bc5702656b2526465bb57c7b77491f5091dce10a
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-03-08 20:04:18 -0600

    blog

--- application/language/spanish/blog_informativo_lang.php
+++ application/language/spanish/blog_informativo_lang.php
@@ -13,20 +13,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * 404 errors
 */
 $lang['blog'] = "Blog";
-$lang['title_question_one'] = "Blog";
-$lang['answer_question_one'] = "Blog";
+$lang['title_question_one'] = "¿Cómo me va a afectar el Brexit si quiero estudiar en Reino Unido?";
+$lang['answer_question_one'] = "Ante los posibles panoramas de resolución del Brexit, las universidades de Reino Unido han tomado precauciones para proteger a sus estudiantes de modificaciones radicales.";
 
-$lang['title_question_two'] = "Blog";
-$lang['answer_question_two'] = "Blog";
+$lang['title_question_two'] = "Beneficios de estudiar en el Reino Unido";
+$lang['answer_question_two'] = "Tener estudios en el extranjero puede abrirte muchas puertas, pero, el país donde lo hagas puede hacer la diferencia.";
 
-$lang['title_question_three'] = "Blog";
-$lang['answer_question_three'] = "Blog";
+$lang['title_question_three'] = "¡Sí!, ahora es mucho más accesible estudiar en el Reino Unido";
+$lang['answer_question_three'] = "La globalización ha impactado también el ámbito educativo, por lo que realizar estudios en el extranjero ahora es más accesible en materia económica y en tiempos de estadía.";
 
-$lang['title_question_four'] = "Blog";
-$lang['answer_question_four'] = "Blog";
+$lang['title_question_four'] = "Estudiar en Reino Unido: ¿qué certificado de inglés necesito?";
+$lang['answer_question_four'] = "Para estudiar en el Reino Unido es necesario presentar una certificación con reconocimiento internacional que valide tu dominio del inglés. Existen diversas maneras de acreditar tus conocimientos, pero es importante conocer sus alcances para elegir la mejor opción.";
 
-$lang['title_question_five'] = "Blog";
-$lang['answer_question_five'] = "Blog";
+$lang['title_question_five'] = "Por qué estudiar en el Reino Unido en 2020";
+$lang['answer_question_five'] = "Estudiar en el Reino Unido es sinónimo de reconocimiento internacional, descubre las facilidades que Anglo Latino pone a tu alcance para hacer de esta meta una realidad.";
 
 $lang['title_question_six'] = "Blog";
 $lang['answer_question_six'] = "Blog";
@@ -43,11 +43,7 @@ $lang['answer_question_nine'] = "Blog";
 $lang['title_question_ten'] = "Blog";
 $lang['answer_question_ten'] = "Blog";
 
-$lang['title_question_one'] = "Blog";
-$lang['answer_question_one'] = "Blog";
 
-$lang['title_question_one'] = "Blog";
-$lang['answer_question_one'] = "Blog";
 
 $lang['english']="English";
 $lang['spanish']="Spanish";
