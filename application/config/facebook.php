f627c10f46b79f7f6d562c35662063ada0fd4851
hectorium110996 <hectori.um.15@gmail.com>
2020-03-13 22:01:39 -0600

    new

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -16,8 +16,8 @@
 |  facebook_graph_version        string   Specify Facebook Graph version. Eg v2.6
 |  facebook_auth_on_load         boolean  Set to TRUE to check for valid access token on every page load.
 */
-$config['facebook_app_id']              = '';
-$config['facebook_app_secret']          = '';
+$config['facebook_app_id']              = '1';
+$config['facebook_app_secret']          = '1';
 $config['facebook_login_type']          = 'web';
 $config['facebook_login_redirect_url']  = 'Login/Login/Facebook';
 $config['facebook_logout_redirect_url'] = 'index.php/login/logout';
