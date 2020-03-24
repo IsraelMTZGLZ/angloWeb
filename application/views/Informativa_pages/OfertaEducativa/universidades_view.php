2971f4ac488a539efef3ff2ecd7b39c4332dea30
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-03-23 22:51:03 -0600

    new

--- application/views/Informativa_pages/OfertaEducativa/universidades_view.php
+++ application/views/Informativa_pages/OfertaEducativa/universidades_view.php
@@ -1,3 +1,6 @@
+    <link rel="stylesheet" type="text/css" href="<?=base_url('resources/assets/Informativa/css/modal_info.css');?>" />
+
+
 
     <div class="page_title_section float_left">
 
@@ -36,12 +39,15 @@
                     <li data-filter=".website"><a href="#">Universidades</a></li>
                     <li data-filter=".design"><a href="#">Preparatorias</a></li>
                     <li data-filter=".ux_ui"><a href="#">Escuela de inglés</a></li>
+                      <li data-filter=".ux_uy"><a href="#">Cursos de verano</a></li>
                 </ul>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="row portfoli_inner">
 
                     <!-- Items -->
+
+
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 website">
                         <div class="portfolio_item">
                             <img src="<?=base_url('resources/assets/Informativa/images/u_bathh.jpg');?>" alt="">
@@ -49,8 +55,13 @@
                                 <a href="#"> University of Bath <span>LEER MÁS </span></a>
                                 <p></p>
                                 <div class="zoom_popup">
-                                    <a class="img-link" href="images/pc1.jpg"> <i class="flaticon-add"></i>
+
+                                    <a> <i class="flaticon-add"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subscribeModal">
+</button></i>
+
                                     </a>
+
+
                                 </div>
                             </div>
             <div class="portfolio_overlay"></div>
@@ -195,7 +206,189 @@
                         <div class="portfolio_item">
                             <img src="<?=base_url('resources/assets/Informativa/images/u_collyers.jpg');?>" alt="">
                             <div class="portfolio_hover">
-                                <a href="#"> University of St Andrews <span>LEER MÁS </span></a>
+                                <a href="#">Collyer´s<span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_bath.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Bath Academy <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_arts.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Cambridge School of Visual and Performing Arts  <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_catso.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> CATS College<span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_aconcord.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Concord College <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_aoncampus.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> On Campus <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_andrew.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> St Andrews College, Cambridge <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  design">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/p_atauton.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#">Taunton School <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_echester.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> English in Chester <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_ebath.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#">Bath Academy of English <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_ehouse.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> International House (Bristol) <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_eselect.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Select English (Cambridge) <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_estanfort.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Stafford House International <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_ecambrige.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> Cambridge Academy of English <span>LEER MÁS </span></a>
+                                <div class="zoom_popup">
+                                    <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
+                                    </a>
+                                </div>
+                            </div>
+                     <div class="portfolio_overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12  ux_ui">
+                        <div class="portfolio_item">
+                            <img src="<?=base_url('resources/assets/Informativa/images/e_estudio.jpg');?>" alt="">
+                            <div class="portfolio_hover">
+                                <a href="#"> The English Studio, London y Dublin <span>LEER MÁS </span></a>
                                 <div class="zoom_popup">
                                     <a class="img-link" href="images/pc8.jpg"> <i class="flaticon-add"></i>
                                     </a>
@@ -209,6 +402,89 @@
                         <a href="#">load more</a>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <img src="<?=base_url('resources/assets/Informativa/images/lu_bath.png');?>" class="rounded mx-auto d-block" alt="...">
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade text-center py-5 subscribeModal-lg "  id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          	<!-- <div class="top-strip"></div> -->
+          <div class="modal-header d-none d-lg-block d-xl-block">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4 "  style="margin-top: -60px;"> <img src="<?=base_url('resources/assets/Informativa/images/lu_bath.png');?>" class="rounded mx-auto d-block" alt="..."></div>
+                <div class="col-md-7 ml-auto"><img  src="<?=base_url('resources/assets/Informativa/images/um_bathh.jpg');?>" alt=""></div>
+              </div>
+            </div>
+            <!-- <a class="h2" href="https://www.fiverr.com/sunlimetech/design-and-fix-your-bootstrap-4-issues" target="_blank"> </a>
+
+            <h3 class="col-md-4 "></h3> -->
+
+        	</div>
+          <div class="modal-body d-none d-lg-block d-xl-block">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4">Fundación: 1966</div>
+                <div class="col-md-4 ">Ciudad de Bath (1hr 50 mins de Londres).</div>
+              </div>
+              <div class="row">
+                <hr>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Ranking:</div>
+                <div class="col-md-4">Ranking Mundial QS 172 The Guardian University Guide Top 10.</div>
+                <div class="col-md-4">Ocupa el 4º lugar general de 122 en la encuesta de satisfacción estudiantil 2018.</div>
+                <br>
+              </div>
+
+
+              <div class="row">
+                <div class="col-md-4">
+                  Facultades:
+
+                    <ul>
+                      <li class="col-md-9 ml-auto" type="disc">Ingeniería y Diseño</li>
+                      <li class="col-md-9 ml-auto" type="disc">Humanidades y Ciencias Sociales</li>
+                      <li class="col-md-9 ml-auto" type="disc">Negocios</li>
+                      <li class="col-md-9 ml-auto" type="disc">Ciencias</li>
+                    </ul>
+
+                </div>
+              </div>
+              <div class="row">
+                <hr>
+              </div>
+              <div class="row">
+                <div class="col-md-4">Ciudad romana, patrimonio mundial de la Humanidad por la UNESCO.</div>
+              </div>
+            </div>
+          </div>
+            <div class="bottom-strip"></div>
         </div>
     </div>
 </div>
