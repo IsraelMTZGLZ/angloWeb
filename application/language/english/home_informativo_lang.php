870c56fff337f221a77642b563495f66a66a98b4
hectorium110996 <hectori.um.15@gmail.com>
2020-04-08 10:23:20 -0500

    home changes

--- application/language/english/home_informativo_lang.php
+++ application/language/english/home_informativo_lang.php
@@ -10,9 +10,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['bienvenido'] = "Welcome";
-$lang['beca_asesoria'] = "We advise you so you can request a Scholarship to study at United Kingdom this 2020.";
+$lang['beca_asesoria'] = "Anglo Latino provides free advice, application information and support right through to scholarship and visa applications.";
 $lang['conocenos'] = "Know us";
-$lang['beca_gratuita'] = "We advise you for FREE and support your education in British institutions and schools of excellence.";
+$lang['beca_gratuita'] = "We offer guidance and advice on applying to scholarships available for entry to UK Universities and colleges in 2020.";
 $lang['nuestros_servicios_home'] = "Our Services";
 $lang['ayudar1'] = "Help Mexicans and Latin Americans of high academic level get enrolled in the best universities";
 $lang['ayudar2'] = "and British schools in order to reach their educational, professional and personal goals.";
@@ -27,9 +27,9 @@ $lang['comunicacion'] = "Comunication";
 $lang['eficacia'] = "Effectiveness";
 $lang['calidad'] = "Quality";
 $lang['univerdiades_home'] = "Universities";
-$lang['univerdiades_desc_home'] = "All the universities with which we have an agreement have internal scholarships for foreign students.";
-$lang['preparatoria_home'] = "High schools";
-$lang['preparatoria_desc_home'] = "All these institutions offer internal scholarships for students.";
+$lang['univerdiades_desc_home'] = "We represent Universities ranked among the top 200 universities in the world, in other words, the top 1% of universities worldwide.";
+$lang['preparatoria_home'] = "Colleges";
+$lang['preparatoria_desc_home'] = "The colleges we represent are focussed on giving students the best possible preparation for university study.";
 $lang['cursos_home'] = "English courses";
 $lang['cursos_desc_home'] = "English courses throughout the year, summer camps for young people; Tailored courses for groups.";
 $lang['cursos__verano_home'] = "Summer courses";
--- application/language/spanish/home_informativo_lang.php
+++ application/language/spanish/home_informativo_lang.php
@@ -10,9 +10,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['leer_mas'] = "Bienvenido";
-$lang['beca_asesoria'] = "Te asesoramos para que puedas solicitar una beca para estudiar en el Reino Unido este 2020.";
+$lang['beca_asesoria'] = "Anglo Latino te asesora por todo el proceso de admision desde proporcionarte informaci&oacute;n acerca de los programas de estudio, hasta guiarte por la aplicaci&oacute;n tanto para la universidad como para las becas disponibles y asesoria en la aplicaci&oacute;n para la visa. Todo de forma gratuita.";
 $lang['conocenos'] = "Conocenos";
-$lang['beca_gratuita'] = "Te asesoramos de manera GRATUITA y apoyamos en tu educaci&oacute;n en instituciones y colegios brit&aacute;nicos de excelencia.";
+$lang['beca_gratuita'] = "Te asesoramos para que puedas solicitar una beca para estudiar en el Reino Unido en el 2020.";
 $lang['nuestros_servicios_home'] = "Nuestros Servicios";
 $lang['ayudar1'] = "Ayudar a mexicanos y latinoamericanos de alto nivel académico a conseguir inscribirse en las mejores universidades";
 $lang['ayudar2'] = "y colegios brit&aacute;nicos con el fin de alcanzar sus metas educativas, profesionales y personales.";
@@ -27,9 +27,9 @@ $lang['comunicacion'] = "Comunicaci&oacute;n";
 $lang['eficacia'] = "Eficacia";
 $lang['calidad'] = "Calidad";
 $lang['univerdiades_home'] = "Universidades";
-$lang['univerdiades_desc_home'] = "Todas las universidades con las que tenemos convenio cuentan con becas internas para estudiantes extranjeros.";
-$lang['preparatoria_home'] = "Preparatoria";
-$lang['preparatoria_desc_home'] = "Todas estas instituciones ofrecen becas internas para estudiantes.";
+$lang['univerdiades_desc_home'] = "Representamos a Universidades rankeadas entre las primeras 200 a nivel mundial, es decir, el top 1% en el mundo por la excelencia de su investigación y su enseñanza.";
+$lang['preparatoria_home'] = "Preparatorias";
+$lang['preparatoria_desc_home'] = "Nuestras preparatorias se enfocan en prepararte para estudiar en las mejores universidades del mundo.";
 $lang['cursos_home'] = "Cursos De Ingl&eacute;s";
 $lang['cursos_desc_home'] = "Cursos de ingl&eacute;s durante todo el año, campamentos de verano para j&oacute;venes; cursos a la medida para grupos.";
 $lang['cursos_home'] = "Cursos De Ingl&eacute;s";
