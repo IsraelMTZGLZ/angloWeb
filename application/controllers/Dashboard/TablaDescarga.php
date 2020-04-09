ad69baf3becd7e6cdeb58ee034c75625af474469
hectorium110996 <hectori.um.15@gmail.com>
2020-04-09 09:46:25 -0500

    tabla

---
+++ application/controllers/Dashboard/TablaDescarga.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TablaDescarga extends MY_RootController {
+
+	public function __construct() {
+        parent::__construct();
+        redirect('Login');
+		if (!@$this->session->userdata('user_sess')->email) {
+			redirect('Login');
+        }
+    }
+
+	public function index()
+	{
+		unset($_SESSION['blog']);
+        $data['user']=$this->session->userdata('user_sess');
+        $this->_initialPage($data);
+        $this->load->view('Dashboard_pages/tablaDescarga_view',$data);
+        $this->_finalPage();
+	}
+	
+}
+ 
