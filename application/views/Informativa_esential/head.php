c35dea27c47ca51f5b3ca716985707fda8ee8eb7
hectorium110996 <hectori.um.15@gmail.com>
2020-04-09 12:21:15 -0500

    test hotjar

--- application/views/Informativa_esential/head.php
+++ application/views/Informativa_esential/head.php
@@ -42,4 +42,15 @@
     <!--favicon-->
     <link rel="shortcut icon" type="image/png" href="<?=base_url('resources/assets/Anglo/LOGOTIPO-AngloLatino.png');?>" />
 
+<!-- Hotjar Tracking Code for My site -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1762491,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
